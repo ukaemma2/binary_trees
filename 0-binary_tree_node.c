f4890bdeeda85d80f21582986d55ed43b196c9c7
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2023-03-30 14:34:26 +0100

    binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,20 +1,31 @@
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+#include "binary_trees.h"
+#include "stdio.h"
 
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: Pointer to the newly created node
+ *         NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
+{
+    binary_tree_t *new;
+
+    new = malloc(sizeof(*new));
+    if (new == NULL)
+        return (NULL); /* insufficient memory*/
+    new->n = value;
+    new->left = new->right = NULL;
+    new->parent = parent;
+
+    if (new->parent == NULL) /*root*/
+        return new;
+    if (new->n < parent->n)
+        parent->left = new;
+    else
+        parent->right = new;
+    return new;
+}
